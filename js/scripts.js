b7599590a08b3213fb5d4d90ab35ce4ba3584f67
Zachary Cipolleta <ZackCipolletta@gmail.com>
2023-01-23 16:50:43 -0800

    add ability to click on location name element and show all inforation about the location

--- js/scripts.js
+++ js/scripts.js
@@ -33,7 +33,7 @@ function displayThings() {
   const place2HTML = document.getElementById('place2')
 
   let placesIveBeen = new PlacesIveBeen;
-  let destination2 = new Destination('NYC', ['Empire State Building'], 'Spring', ['good food']);
+  // let destination2 = new Destination('NYC', ['Empire State Building'], 'Spring', ['good food']);
   let destination1 = new Destination('Seattle', ['Space Needle'], 'Summer', ['vacation']);
   placesIveBeen.addDestination(destination1);
   // placesIveBeen.addDestination(destination2);
@@ -41,23 +41,29 @@ function displayThings() {
   place1HTML.append(divEl);
   place1HTML.firstChild.innerText = placesIveBeen.location[1].location;
   place1HTML.id = placesIveBeen.location[1].location;
-  document.getElementById(placesIveBeen.location[1].location).addEventListener('click', location1Func);
 
-  // place2HTML.append(divEl);
-  // place2HTML.firstChild.innerText = placesIveBeen.location[2].location;
+  document.getElementById(placesIveBeen.location[1].location).addEventListener('click', location1Func);
+  return place1HTML.id = placesIveBeen.location[1].location;
 
 }
 
 function showDetails() {
-  document.getElementById("NYC").addEventListener("click", location1Func)
+  document.getElementById("Seattle").addEventListener("click", location1Func)
   // document.getElementById("Seattle").addEventListener("click", location2Func)
 }
 
 function location1Func(){
-  let placesIveBeen2 = placesIveBeen.location[1];
-  let liElement = document.createElement('li');
-  liElement.appendChild(placesIveBeen2);
-  document.getElementById('NYC').appendChild(liElement);
+  let placesIveBeen = new PlacesIveBeen;
+  let destination1 = new Destination('Seattle', ['Space Needle'], 'Summer', ['vacation']);
+  placesIveBeen.addDestination(destination1);
+  let placesIveBeen2LM = placesIveBeen.location[1].landmarks;
+  let placesIveBeen2Time = placesIveBeen.location[1].timeOfYear;
+  let destinationElement = document.getElementById('Seattle');
+  for(const [key, value] of Object.entries(destination1)) {
+    let liElement = document.createElement('li');
+    liElement.innerText = (`${key}: ${value}`);
+    destinationElement.append(liElement);
+  }
 }
 
 
